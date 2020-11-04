729b8d41ede772c49f56fa12b959e66162e345f0
tim <timon0596@yandex.ru>
2020-11-05 02:42:33 +0300

    game over

--- app/app.vue
+++ app/app.vue
@@ -2,43 +2,78 @@
 .main
   .buttons
     .button(v-for='(button,index) in buttons' :class='button' :key='index' @click='onClick(index)')
+  .start(@click='start') start
+  .start(@click='stop') stop
+  .start {{round}}
+  .start(v-if='isWrong') game over
 </template>
 <script>
 export default{
   data(){
     return {
       buttons:['r','g','b','o'],
-      round:5,
+      round:2,
       finished:true,
       sequence:[],
       clientSequence:[],
       buttonsElements: [],
       promises:[],
       delay: 1,
-      isWrong:false
+      isWrong:false,
+      isPending:false,
+      stopped:false,
+      success:false,
+      isChecking:false
     }
   },
   methods:{
+    stop(){
+      this.stopped=true
+    },
+    start(){
+
+      if(this.isPending) return
+      this.promises=[]
+      this.clientSequence=[]
+      this.stopped=false
+      this.isPending=true
+      this.isWrong=false
+      this.generateSequence()
+      this.execSequence()
+    },
     generateSequence(){
       this.sequence=[]
-      this.clientSequence=[]
+      
       for(let i =0;i<this.round;i++){
         this.sequence.push(this.rand())
       }
-      console.log(this.sequence)
     },
     rand(){
       return Math.round(Math.random()*3)
     },
     onClick(i){
-      this.clientSequence.push(i)
-      this.isWrong=this.clientSequence.some((el,i)=>el!==this.sequence[i])
+      if(this.isPending||this.isWrong||!this.isChecking) return
+        this.clientSequence.push(i)
+      if(this.clientSequence.length<this.sequence.length){
+        this.isWrong=this.clientSequence.some((el,i)=>el!==this.sequence[i])
+        if(this.isWrong){
+          this.round=1
+        }
+      }else if(!this.isWrong){
+        this.success=true
+        this.isChecking=false
+        this.round++
+      }
+      
     },
     execSequence(){
       this.sequence.forEach((el,i)=>{ 
 
-        const promise = ()=>new Promise((res)=>{
-
+        const promise = ()=>new Promise((res,rej)=>{
+          if(this.stopped) {
+            rej('err')
+            return
+          }
           this.buttonsElements[el].classList.add('button_active')
 
           setTimeout(()=>{
@@ -52,13 +87,20 @@ export default{
         this.promises.push(promise)
       })
       this.promises.reduce((acc,current)=>acc.then(current),Promise.resolve())
+      .then(()=>{
+        this.isPending=false
+        this.isChecking=true
+      })
+      .catch((e)=>{
+
+        this.isPending=false
+        this.isChecking=false
+      })
       
     }
   },
   mounted(){
     this.buttonsElements=this.$el.children[0].children
-    this.generateSequence()
-    this.execSequence()
   }
 }
 </script>
@@ -90,4 +132,12 @@ export default{
       background: blue
     .o
       background: orange
+  .start
+    padding: 10px 20px
+    border: 1px solid green
+    color: green
+    border-radius: 5px
+    cursor: pointer
+    &:active
+      opacity: .5
 </style>
